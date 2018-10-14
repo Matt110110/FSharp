db1c487aea5fb915c752c9ff8aad212baf58f109
Matt110110 <matruprasadchand@gmail.com>
2018-10-14 23:27:34 +0530

    Broiler plate code with errors. Program needs to be built from this.

---
+++ Capstone01/FuelManagement/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    while true do
+        try
+            let destination = getDestination()
+            printfn "Trying to drive to %s" destination
+            petrol <- driveTo(petrol, destination)
+            printfn "Made it to %s! You have %f petrol left" destination petrol
+        with ex -> printfn "ERROR: %s" ex.Message
+    0 // return an integer exit code
