c2dbe19bef5f8438af575cba32be3c565fb24736
Matt110110 <matruprasadchand@gmail.com>
2018-10-15 00:17:55 +0530

    Program running in infinite loop

--- Capstone01/FuelManagement/Car.fs
+++ Capstone01/FuelManagement/Car.fs
@@ -1,2 +1,25 @@
 ﻿module Car
 
+type Destinations =
+    | Home = 0
+    | Petrol = 1
+    | Stadium = 2
+    | Office = 3
+    | Unknown = 4
+
+// Function to return the distance to travel
+let getDistance destination =
+    if destination = Destinations.Petrol then 10
+    elif destination = Destinations.Home then 25
+    elif destination = Destinations.Office then 50
+    elif destination = Destinations.Stadium then 25
+    else failwith "Unknown destination"
+
+let calculateRemainingPetrol (currentPetrol:int, distance:int) = 
+    if currentPetrol >= distance then currentPetrol - distance
+    else failwith "Oops! You have run out of petrol!"
+
+let driveTo (petrol, destination) =
+    let petrolAfterDriving = calculateRemainingPetrol(petrol, getDistance destination)
+    if destination = Destinations.Petrol then petrolAfterDriving + 50
+    else petrolAfterDriving
--- Capstone01/FuelManagement/Program.fs
+++ Capstone01/FuelManagement/Program.fs
@@ -1,27 +1,29 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
-type Destinations =
-    | Home = 0
-    | Petrol = 1
-    | Stadium = 2
-    | Office = 3
-    | Unknown = 4
+open System
+open Car
 
-// Function to return the distance to travel
-let getDistance destination =
-    if destination = Destinations.Petrol then 10
-    elif destination = Destinations.Home then 25
-    elif destination = Destinations.Office then 50
-    elif destination = Destinations.Stadium then 25
-    else failwith "Unknown destination"
+let getDestinationString =
+    Console.Write("Enter destination: ")
+    Console.ReadLine()
+
+let getProperDestination destination =
+    if destination = "Petrol" then Destinations.Petrol
+    elif destination = "Stadium" then Destinations.Stadium
+    elif destination = "Office" then Destinations.Office
+    elif destination = "Home" then Destinations.Home
+    else failwith "Unknown destination!"
+
+let mutable petrol = 100
 
 [<EntryPoint>]
 let main argv = 
     while true do
         try
-            let destination = getDestination()
-            printfn "Trying to drive to %s" destination
+            let dest = getDestinationString
+            let destination = getProperDestination dest
+            printfn "Trying to drive to %s" dest
             petrol <- driveTo(petrol, destination)
-            printfn "Made it to %s! You have %f petrol left" destination petrol
+            printfn "Made it to %s! You have %d petrol left" dest petrol
         with ex -> printfn "ERROR: %s" ex.Message
     0 // return an integer exit code
