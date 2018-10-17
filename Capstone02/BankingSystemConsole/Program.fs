90fb1fb0b01f8ef4c9d4c3bdea430ff973dd37df
Matt110110 <matruprasadchand@gmail.com>
2018-10-17 16:18:45 +0530

    Main logic complete. Minor bugs remain.

--- Capstone02/BankingSystemConsole/Program.fs
+++ Capstone02/BankingSystemConsole/Program.fs
@@ -1,8 +1,41 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open BankAccountSystemAPI
 
-open System
+// Take input from the console and store it in file
+let private getAccountDetails() = 
+    printfn "Enter the account number: "
+    let accNo = Console.ReadLine()
+    printfn "Enter the account owner details: "
+    printfn "Enter the First name: "
+    let fname = Console.ReadLine()
+    printfn "Enter the last name: "
+    let lname = Console.ReadLine()
+    printfn "Enter the age: "
+    let age = Console.ReadLine() |> int
+    let owner = { Firstname = fname; Lastname = lname; Age = age }
+    printfn "\nEnter the ammount in the account: "
+    let amt = Console.ReadLine() |> decimal
+    { UniqueID = accNo; Owner = owner; CurrentBalance = amt }
+
+let amt() =
+    printfn "Enter the ammount"
+    Console.ReadLine() |> decimal
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    Console.WriteLine("Welcome to the bank\n")
+    let mutable account = getAccountDetails()
+    let mutable choice = true
+    let mutable amt = amt()
+    while choice do
+        printfn "\n\nPlease select your preferred operation:"
+        printfn "1. Deposit cash"
+        printfn "2. Withdraw cash"
+        printfn "3. Exit"
+        let mutable ch = Console.ReadLine() |> int
+        match ch with
+            | 1 -> account <- Audit.depositWithConsoleAudit amt account
+            | 2 -> account <- Audit.withdrawWithConsoleAudit amt account
+            | 3 -> choice <- false
+            | _ -> printfn "Please enter a valid choice"
     0 // return an integer exit code
